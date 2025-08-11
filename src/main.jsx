6cbb04a310a7b31d5a216d598c658f49ec976e98
Zulqarnain khan <70657074+zulqarnain902@users.noreply.github.com>
2025-08-11 20:33:41 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './styles.css'
+import './src/styles.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
