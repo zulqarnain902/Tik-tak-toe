f28717fca677350a2b051f9094b855750df149d5
Zulqarnain khan <70657074+zulqarnain902@users.noreply.github.com>
2025-08-11 21:14:19 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './src/App'
 import './src/styles.css'
 
 createRoot(document.getElementById('root')).render(
